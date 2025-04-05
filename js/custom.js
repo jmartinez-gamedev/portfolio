580499fbf56f9465aadf37b9d3bb26e80212afbd
DESKTOP-SDFA1BG\Shovi Medo <jmartinez.gamedev@gmail.com>
2025-04-05 17:49:14 +0200

    First commit with actual content.

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,93 @@
+function loadHeader() {
+    fetch('header.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to load header');
+            }
+            return response.text();
+        })
+        .then(data => {
+            if (headerinsert = document.getElementById('headerinsert')) {
+                headerinsert.outerHTML = data;
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+function loadFooter() {
+    fetch('footer.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to load footer');
+            }
+            return response.text();
+        })
+        .then(data => {
+            if (footerinsert = document.getElementById('footerinsert')) {
+                footerinsert.outerHTML = data;
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-    // Select the video element
+    loadHeader();
+    loadFooter();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+
     const backgroundVideo = document.querySelector('.scrollblur');
 
-    // Listen for the scroll event
-    window.addEventListener('scroll', () => {
-        // Check the scroll position
-        if (window.scrollY > 50) { // You can adjust this value to trigger at a specific scroll position
-            backgroundVideo.classList.add('blur');
-        } else {
-            backgroundVideo.classList.remove('blur');
-        }
-    });
+    if (backgroundVideo) {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 50) {
+                backgroundVideo.classList.add('blur');
+            } else {
+                backgroundVideo.classList.remove('blur');
+            }
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const textContainer = document.getElementById('job-title');
+    const innerText = document.getElementById('job-title-text');
+    const texts = [
+        "Product Owner",
+        "Senior Game Designer",
+        "Senior Technical Designer",
+        "Senior Gameplay Programmer"
+    ];
+    let currentIndex = 0;
+
+    if (textContainer && innerText) {
+        innerText.textContent = texts[0];
+
+        setInterval(() => {
+            textContainer.classList.add('hidden');
+            setTimeout(() => {
+                currentIndex = (currentIndex + 1) % texts.length;
+                innerText.textContent = texts[currentIndex];
+                textContainer.classList.remove('hidden');
+            }, 1000);
+        }, 4000);
+
+        
+        setInterval(() => {
+            if(innerText.textContent.endsWith("."))
+            {
+                innerText.textContent = innerText.textContent.slice(0, -1);
+            }
+            else
+            {
+                innerText.textContent = innerText.textContent + ".";
+            }
+        }, 500);
+    }
 });
